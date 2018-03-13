d0e003de316dc9662e31062cde6bcd11a30bf253
mikewkozak <mwk24@drexel.edu>
2018-03-13 17:47:12 -0400

    Adding zerg tech tree

--- nova/SCTechTreeManager/ZergTreeManager.cpp
+++ nova/SCTechTreeManager/ZergTreeManager.cpp
@@ -34,32 +34,100 @@ void ZergTreeManager::buildTree() {
 	printf("buildTree() - creating root\n");
 
 	VertexDescriptor hatchery = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Hatchery, "Hatchery", 1);
+	VertexDescriptor extractor = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Extractor, "Extractor", 1);
+	VertexDescriptor creep_colony = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Creep_Colony, "Creep Colony", 1);
 
 
 	//depth1
 	printf("buildTree() - creating depth1\n");
-	VertexDescriptor barracks = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Terran_Barracks, "Barracks", 1);
-	boost::add_edge(hatchery, barracks, 1, techTree);
+	VertexDescriptor spawning_pool = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Spawning_Pool, "Spawning Pool", 1);
+	VertexDescriptor evolution_chamber = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Evolution_Chamber, "Evolution Chamber", 1);
+	boost::add_edge(hatchery, spawning_pool, 1, techTree);
+	boost::add_edge(hatchery, evolution_chamber, 1, techTree);
+
+	VertexDescriptor larva = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Larva, "Larva", 1);
+	VertexDescriptor drone = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Drone, "Drone", 1);
+	VertexDescriptor overlord = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Overlord, "Overlord", 1);
+	boost::add_edge(hatchery, larva, 1, techTree);
+	boost::add_edge(hatchery, overlord, 1, techTree);
+	boost::add_edge(hatchery, drone, 1, techTree);
 
 	
 
 	//depth2
 	printf("buildTree() - creating depth2\n");
+	VertexDescriptor spore_colony = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Spore_Colony, "Spore Colony", 1);
+	boost::add_edge(evolution_chamber, spore_colony, 1, techTree);
+
+	VertexDescriptor lair = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Lair, "Lair", 1);
+	VertexDescriptor hydralisk_den = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Hydralisk_Den, "Hydralisk Den", 1);
+	VertexDescriptor sunken_colony = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Sunken_Colony, "Sunken Colony", 1);
+	boost::add_edge(spawning_pool, lair, 1, techTree);
+	boost::add_edge(spawning_pool, hydralisk_den, 1, techTree);
+	boost::add_edge(spawning_pool, sunken_colony, 1, techTree);
+
+	VertexDescriptor zergling = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Zergling, "Zergling", 1);
+	boost::add_edge(spawning_pool, zergling, 1, techTree);
 	
 
 	//depth3
 	printf("buildTree() - creating depth3\n");
-	
+	VertexDescriptor queens_nest = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Queens_Nest, "Queen's Nest", 1);
+	VertexDescriptor spire = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Spire, "Spire", 1);
+	boost::add_edge(lair, queens_nest, 1, techTree);
+	boost::add_edge(lair, spire, 1, techTree);
+
+	VertexDescriptor hydralisk = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Hydralisk, "Hydralisk", 1);
+	boost::add_edge(hydralisk_den, hydralisk, 1, techTree);
 
 	//depth 4
 	printf("buildTree() - creating depth4\n");
-	
+	VertexDescriptor hive = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Hive, "Hive", 1);
+	VertexDescriptor infested_command_center = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Infested_Command_Center, "Infested Command Center", 1);
+	boost::add_edge(queens_nest, hive, 1, techTree);
+	boost::add_edge(queens_nest, infested_command_center, 1, techTree);
+
+
+	VertexDescriptor queen = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Queen, "Queen", 1);
+	VertexDescriptor broodling = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Broodling, "Broodling", 1);
+	boost::add_edge(queens_nest, queen, 1, techTree);
+	boost::add_edge(queens_nest, broodling, 1, techTree);
+
+	VertexDescriptor mutalisk = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Mutalisk, "Mutalisk", 1);
+	boost::add_edge(spire, mutalisk, 1, techTree);
+
 
 	//depth 5
 	printf("buildTree() - creating depth5\n");
-	
+	VertexDescriptor ultralisk_cavern = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Ultralisk_Cavern, "Ultralisk Cavern", 1);
+	VertexDescriptor greater_spire = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Greater_Spire, "Greater Spire", 1);
+	VertexDescriptor nydus_canal = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Nydus_Canal, "Nydus Canal", 1);
+	VertexDescriptor defiler_mound = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Defiler_Mound, "Defiler Mound", 1);
+	boost::add_edge(hive, ultralisk_cavern, 1, techTree);
+	boost::add_edge(hive, greater_spire, 1, techTree);
+	boost::add_edge(hive, nydus_canal, 1, techTree);
+	boost::add_edge(hive, defiler_mound, 1, techTree);
 
-	//Calculate depth for all nodes
+
+	VertexDescriptor infested_terran = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Infested_Terran, "Infested Terran", 1);
+	boost::add_edge(infested_command_center, infested_terran, 1, techTree);
+
+
+	//depth 6
+	VertexDescriptor ultralisk = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Ultralisk, "Ultralisk", 1);
+	boost::add_edge(ultralisk_cavern, ultralisk, 1, techTree);
+
+
+	VertexDescriptor guardian = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Guardian, "Guardian", 1);
+	VertexDescriptor devourer = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Devourer, "Devourer", 1);
+	boost::add_edge(greater_spire, guardian, 1, techTree);
+	boost::add_edge(greater_spire, devourer, 1, techTree);
+	
+	VertexDescriptor defiler = GraphUtils::addNode(techTree, BWAPI::UnitTypes::Zerg_Defiler, "Defiler", 1);
+	boost::add_edge(defiler_mound, defiler, 1, techTree);
+
+
+	//Calculate depth for all nodes	
 	dijkstra_shortest_paths(techTree, hatchery, boost::weight_map(boost::make_constant_property<EdgeDescriptor>(1)).distance_map(get(&Vertex::depth, techTree)));
 
 }
